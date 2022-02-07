b467e40c581eb689548895c33bbad1c153558e6d
systemuser <admin@enagednation.com>
2022-02-07 09:35:27 +0000

    Updating template for new reach client

--- RoboFile.php
+++ RoboFile.php
@@ -2,13 +2,13 @@
 /**
  * Contains RoboFile
  *
- * @package marchmania
+ * @package mohawk-mm
  */
 
 /**
  * @author Joshua Copeland <josh@engagednation.com>
  * @author Matthew Kosolofski <matthew.kosolofski@engagednation.com>
- * @package marchmania
+ * @package mohawk-mm
  */
 class RoboFile extends \Robo\Tasks
 {
